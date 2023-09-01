dd7650c3174b36607df62f49e668580b672f1794
Nelia11 <levit.nelia@icloud.com>
2023-09-01 18:51:06 +0200

    Update page layout

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,20 +5,18 @@ import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponent
 import { LeftSection } from './HeroStyles';
 
 const Hero = () => (
-  <div>
-    <Section row nopadding>
-      <LeftSection>
-        <SectionTitle main center>
-          Hi, I'm Nelia <br />
-          A Full Stack Developer
-        </SectionTitle>
-        <SectionText>
-          My journey involves crafting dynamic web applications using technologies like JavaScript, React, Node.js, and MySQL
-        </SectionText>
-        {/* <Button onClick={() => window.location = "https://www.google.dk/?gws_rd=ssl"}>Learn More</Button> */}
-      </LeftSection>
-    </Section>
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Hi, I'm Nelia <br />
+        A Full Stack Developer
+      </SectionTitle>
+      <SectionText>
+        My journey involves crafting dynamic web applications using technologies like JavaScript, React, Node.js, and MySQL
+      </SectionText>
+      {/* <Button onClick={() => window.location = "https://www.google.dk/?gws_rd=ssl"}>Learn More</Button> */}
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
