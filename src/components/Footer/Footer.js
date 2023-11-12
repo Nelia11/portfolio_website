6f41f6bf063d06d61086bb7e86d101b4cfe85142
Nelia11 <levit.nelia@icloud.com>
2023-11-12 12:17:18 +0100

    Modify layout of footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,33 +2,49 @@ import React from 'react';
 import { AiFillGithub, AiFillFacebook, AiFillLinkedin } from 'react-icons/ai';
 
 import { SocialIcons } from '../Header/HeaderStyles';
-import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './FooterStyles';
+import {
+  CompanyContainer,
+  FooterWrapper,
+  LinkItem,
+  Slogan,
+  SocialContainer,
+} from './FooterStyles';
 
 const Footer = () => {
   return (
     <FooterWrapper>
       <CompanyContainer>
         <Slogan>Write me an email </Slogan>
-        <LinkItem href="mailto:levit.nelia@icloud.com">
+        <LinkItem href='mailto:levit.nelia@icloud.com'>
           levit.nelia@icloud.com
         </LinkItem>
       </CompanyContainer>
-      <SocialIconsContainer>
-        <CompanyContainer>
-          <Slogan>Or reach me via</Slogan>
-        </CompanyContainer>
+      <CompanyContainer>
+        <Slogan>Or reach me via</Slogan>
         <SocialContainer>
-          <SocialIcons href="https://github.com/Nelia11" target="_blank" rel="noopener noreferrer">
-            <AiFillGithub size="3rem"/>
+          <SocialIcons
+            href='https://github.com/Nelia11'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <AiFillGithub size='3rem' />
           </SocialIcons>
-          <SocialIcons href="https://www.linkedin.com/in/nelia-levit-03144a234/" target="_blank" rel="noopener noreferrer">
-            <AiFillLinkedin size="3rem"/>
+          <SocialIcons
+            href='https://www.linkedin.com/in/nelia-levit-03144a234/'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <AiFillLinkedin size='3rem' />
           </SocialIcons>
-          <SocialIcons href="https://www.facebook.com/profile.php?id=100008215662863" target="_blank" rel="noopener noreferrer">
-            <AiFillFacebook size="3rem"/>
+          <SocialIcons
+            href='https://www.facebook.com/profile.php?id=100008215662863'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <AiFillFacebook size='3rem' />
           </SocialIcons>
         </SocialContainer>
-      </SocialIconsContainer>
+      </CompanyContainer>
     </FooterWrapper>
   );
 };
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,20 +1,20 @@
 export const projects = [
   {
-    title: 'Meal-sharing App',
-    description: "Dynamic Full Stack Application for sharing meals built with the PERN stack.  It features meal listings, a reservation system, ratings, reviews, and robust search and filter options for a personalized experience.",
-      image: '/images/1.png',
-      tags: ['Postgres', 'Knex', 'Express', 'React', 'Node'],
-    source: 'https://meal-sharing-ulm8.onrender.com/',
-    code: 'https://github.com/Nelia11/meal-sharing',
-    id: 0
-  },
-  {
     title: 'E-learning',
     description: "E- learning app is a collaborative team project that empowers mentors to create and offer courses, provides student enrollment and access, and features user authentication and content management.",
       image: '/images/2.png',
       tags: ['Postgres', 'Firebase', 'Node', 'React'],
     source: 'https://erikson-webservice.onrender.com/add-course',
     code: 'https://github.com/HackYourFuture-CPH/class-25-erikson',
+    id: 0
+  },
+  {
+    title: 'Meal-sharing App',
+    description: "Dynamic Full Stack Application for sharing meals built with the PERN stack.  It features meal listings, a reservation system, ratings, reviews, and robust search and filter options for a personalized experience.",
+      image: '/images/1.png',
+      tags: ['Postgres', 'Knex', 'Express', 'React', 'Node'],
+    source: 'https://meal-sharing-ulm8.onrender.com/',
+    code: 'https://github.com/Nelia11/meal-sharing',
     id: 1
   }
 ];
