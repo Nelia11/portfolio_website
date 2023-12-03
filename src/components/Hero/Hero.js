c8af48fbc9b733f14edb4a61bc531ba18cec1c78
Nelia11 <levit.nelia@icloud.com>
2023-12-03 17:45:50 +0100

    Minor fix

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from '../../styles/GlobalComponents';
 // import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
@@ -8,11 +12,11 @@ const Hero = () => (
   <Section row nopadding>
     <LeftSection>
       <SectionTitle main center>
-        Hi, I'm Nelia <br />
-        A Full Stack Developer
+        Hi, I'm Nelia <br />A Frontend Developer
       </SectionTitle>
       <SectionText>
-        My journey involves crafting dynamic web applications using technologies like JavaScript, React, Node.js, and PostgreSQL
+        My journey involves crafting dynamic web applications using technologies
+        like JavaScript, React, Node.js, and PostgreSQL
       </SectionText>
       {/* <Button onClick={() => window.location = "https://www.google.dk/?gws_rd=ssl"}>Learn More</Button> */}
     </LeftSection>
